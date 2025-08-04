469d7f1a3f4a8144821c9d758a8962452808d035
HuyPham <huy9008437@gmail.com>
2025-08-04 17:01:53 +0700

    chore(app): setup express app and use auth routes

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,31 @@
+// main.ts hoặc index.ts
 import express from 'express'
-import databaseService from './config/database.service'
+import DatabaseService from './config/database.service'
 import { getEnvConfig } from './config/getEnvConfig'
-import { ObjectId } from 'mongodb'
-getEnvConfig()
-const app = express()
-databaseService.messages.insertOne({
-  _id: new ObjectId(),
-  conversation_id: new ObjectId(),
-  conversation_type: 'direct',
-  sender_id: new ObjectId(),
-  content: 'Hello, world!',
-  media: [],
-  send_at: new Date(),
-  read_by: []
-})
+import { authRouter } from './modules'
 
-app.listen(3000, () => {
-  console.log('success')
-})
+const main = async () => {
+  getEnvConfig()
+  const app = express()
+  const PORT = process.env.PORT || 3000
+
+  const databaseService = new DatabaseService()
+
+  try {
+    await databaseService.connect()
+    console.log('Database connected successfully')
+
+    // Sau khi kết nối DB xong thì mới khởi động app
+    app.use(express.json())
+    app.use('/auth', authRouter)
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`)
+    })
+  } catch (error) {
+    console.error('Error connecting to the database:', error)
+    process.exit(1) // thoát app, không chạy nữa
+  }
+}
+
+main()
