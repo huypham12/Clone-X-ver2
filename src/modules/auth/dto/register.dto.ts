86bde45d0531b786bc0347926afafea3325d4575
HuyPham <huy9008437@gmail.com>
2025-08-04 17:01:47 +0700

    feat(dto): add DTOs for register request

---
+++ src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,27 @@
+import { SuccessResponseDto } from '~/common/success-response.dto'
+import { HTTP_STATUS } from '~/constants/httpStatus'
+import { authMessage } from '~/constants/messages'
+
+// REQUEST DTO
+export class RegisterBodyDto {
+  constructor(
+    public name: string,
+    public email: string,
+    public password: string,
+    public confirm_password: string,
+    public date_of_birth: string
+  ) {}
+}
+
+// RESPONSE DTO
+export interface RegisterData {
+  access_token: string
+  refresh_token: string
+  email_verify_token: string
+}
+
+export class RegisterResponseDto extends SuccessResponseDto<RegisterData> {
+  constructor(data: RegisterData) {
+    super(HTTP_STATUS.CREATED, authMessage.REGISTER_SUCCESS, data)
+  }
+}
